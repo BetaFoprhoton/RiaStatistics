ca207a82109c9bec025d454c1c5ab6e017a75b24
BetaFoprhoton <96339528+BetaFoprhoton@users.noreply.github.com>
2024-08-17 17:55:52 +0800

    加入了往年信息的统计
    still have bugs

--- src/RiaStatisticsMain.py
+++ src/RiaStatisticsMain.py
@@ -14,6 +14,7 @@ colors = ['#3cdc3c', '#ffc864', '#ff2700', '#686868']
 op_list = ['鱼', '星', '灯', '御', '桶', '阿', '影', '冰', '鲈']
 
 baseurl = "https://wiki.ria.red/wiki/%E5%AE%A1%E6%A0%B8%E8%AE%B0%E5%BD%95%E9%A1%B5"
+beforeUrl = "https://wiki.ria.red/wiki/%E5%AE%A1%E6%A0%B8%E8%AE%B0%E5%BD%95%E9%A1%B5(202202~202209)"
 def askURL(url):
     head = {"User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"}
     request = urllib.request.Request(url, headers=head)
@@ -28,8 +29,8 @@ def askURL(url):
             print(e.reason)
     return html
 
-def get_results_list():
-    html = askURL(baseurl)
+def get_results_list(url = baseurl):
+    html = askURL(url)
     soup = BeautifulSoup(html, "html.parser")
     all_results = []
     for item in soup.find_all('table', class_="wikitable"):
@@ -40,11 +41,16 @@ def get_results_list():
             results = re.findall(r'<td>(.*?)</td>', results_string)
             if not results:
                 continue
+            try:
+                if int(results[0]) <= 1919:
+                    break
+            except:
+                continue
             all_results.append(results)
     return all_results
 
 
-def test_st(results_list=None) -> dict:
+def test_st(results_list = None) -> dict:
     if results_list is None:
         results_list = get_results_list()
     results_dict = {"通过": 0, "重试": 0, "未通过": 0, "无效": 0}
@@ -60,33 +66,40 @@ def test_st(results_list=None) -> dict:
             case '-': results_dict["无效"] += 1
     return results_dict
 
-def info_st(results_list = get_results_list()) -> dict:
+def info_st(results_list=None) -> dict:
+    if results_list is None:
+        results_list = get_results_list()
     results_dict = {}
     for result in results_list:
         try:
             result[3]
         except:
             continue
+        small = re.findall(r'(<small>(.*?)</small>)', result[3])
         sub = re.findall(r'(<sub>(.*?)</sub>)', result[3])
-
-        if sub != []:
-            sub = ''.join(sub[0][0])
-            name = result[3].replace(sub, "")
-        else: name = result[3]
+        try:
+            if small != []: # do not believe compiler
+                small = ''.join(small[0][0])
+                name = result[3].replace(small, "")
+            elif sub != []:
+                sub = ''.join(sub[0][0])
+                name = result[3].replace(sub, "")
+            else: name = result[3]
+        except:
+            continue
         try:
             results_dict[name] += 1
         except:
             results_dict[name] = 1
     return dict(sorted(results_dict.items(), key=lambda item: item[1], reverse=True))
 
-def op_st() -> dict:
-    baseurl = "https://wiki.ria.red/wiki/%E5%AE%A1%E6%A0%B8%E8%AE%B0%E5%BD%95%E9%A1%B5"
-    html = askURL(baseurl)  # 保存获取到的网页源码
+def op_st(url = baseurl) -> dict:
+    html = askURL(url)  # 保存获取到的网页源码
     soup = BeautifulSoup(html, "html.parser")
     all_results = []
     for item in soup.find_all('table', class_="wikitable"):  # 查找符合要求的字符串
         item = str(item)
-        item = ''.join(re.findall(r'[^\n]', item))
+        item = ''.join(re.findall(r'[^\n&]', item))
         all_results += re.findall(r'<th>检票员：(.+?)</th>', item)  # 通过正则表达式查找
         op_dict = {}
         for char in op_list:
@@ -97,7 +110,9 @@ def op_st() -> dict:
 
     return dict(sorted(op_dict.items(), key=lambda item: item[1], reverse=True))
 
-def sub_st(results_list = get_results_list()) -> dict:
+def sub_st(results_list=None) -> dict:
+    if results_list is None:
+        results_list = get_results_list()
     results_dict = {}
     for result in results_list:
         try:
@@ -119,7 +134,7 @@ def sub_st(results_list = get_results_list()) -> dict:
 
 def show_pie(results_dict: dict, title = "统计"):
     plt.pie(x=results_dict.values(),  # 绘图数据
-            labels=results_dict.keys(),  #标签
+            labels=results_dict.keys(),  # 标签
             autopct='%.2f%%',  # 设置百分比的格式，保留两位小数
             pctdistance=0.8,  # 设置百分比标签与圆心的距离
             labeldistance=1.2,  # 设置标签与圆心的距离
@@ -137,10 +152,16 @@ def show_pie(results_dict: dict, title = "统计"):
 
 
 show_pie(op_st(), "Ria检票员活跃数统计")
-show_pie(test_st(), "Ria测试数统计")
+results_List = get_results_list(baseurl)
+show_pie(test_st(), "Ria测试统计")
 show_pie(info_st(), "Ria备注信息统计")
-show_pie(sub_st(), "Ria未通过者小标题原因统计")
+show_pie(sub_st(), "Ria小标题统计")
 
+show_pie(op_st(beforeUrl), "往年Ria检票员活跃数统计")
+results_list = get_results_list(beforeUrl)
+show_pie(test_st(results_list), "往年Ria测试统计")
+#show_pie(info_st(results_list), "往年Ria备注信息统计") # bugs here but i don't wanna fix it
+show_pie(sub_st(results_list), "往年Ria小标题统计")
 
 '''
 鱼	鳕鱼	Codusk
